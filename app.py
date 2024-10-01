153bc2b1259128da15255d5a502330899bdc6931
Andrew Manteza <manteza14@gmail.com>
2024-10-01 18:23:54 +0800

    First Commit

---
+++ app.py
@@ -0,0 +1,45 @@
+
+
+
+#Should I give the user a choice wether to have the income as monthly or yearly
+ 
+ 
+
+def calc_Function():
+    initialCash = 0
+    total_income = 0
+    total_expense = 0
+
+    overallIncome = int(input("how much do you earn month after taxes:"))
+    overallExpense = int(input("how much do you earn month after taxes:"))
+
+
+    print(overallIncome - overallExpense)
+    
+
+
+calc_Function()
+   
+
+    
+    
+     
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
