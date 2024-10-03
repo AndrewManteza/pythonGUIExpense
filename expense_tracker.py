0edb259de1d1b8eb3d10f8bc551856c704a3edcd
Andrew Manteza <manteza14@gmail.com>
2024-10-03 19:47:39 +0800

    added logic for user input

--- expense_tracker.py
+++ expense_tracker.py
@@ -13,24 +13,53 @@ def main():
     # Read the file and summarize expenses.
     summarize_user_expense()
 
-    pass
-
-
-
+    
 
 
 def get_user_expense():
     print(f" Get user expense")
-    pass
+    expense_name = input('Enter expense name: ')
+    expense_amount = float(input('Enter expense amount: '))
+    print(f"You have entered {expense_name}. {expense_amount}")
+
+
+    expense_categories = [
+        'Food', 'Home', 'Work', 'Fun', 'Misc'
+    ]
+
+    while True:
+        print('Select a category: ' )
+        #Loops to show how many categories we previously defined
+        for i, category_name in enumerate(expense_categories):
+            print(f' {i + 1}, {category_name}')
+
+
+        #variable for storing categories 
+        value_range = f"[1 - {len(expense_categories)}]"
+
+
+        #Gets user input on what category number to choose
+        selected_index = int(input(f"Enter a category number: {value_range}: ")) -1 
+
+
+        #checks if user input is within the expense category length
+
+        if selected_index in range(len(expense_categories)):
+            break
+        else:
+            print("Invalid Category. Please Try again!")
+
+
+            
+    
 
 def save_user_expense():
-    print(f" Get user expense")
-    pass
-
+    print(f" Save user expense")
+   
 
 def summarize_user_expense():
-    print(f" Get user expense")
-    pass
+    print(f" Summarize user expense")
+   
 
 
 
