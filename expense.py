b22b5f67c67b5cf3150f6c6a63a6e3d719f2b477
Andrew Manteza <manteza14@gmail.com>
2024-10-08 18:06:09 +0800

    added more logic for expense calculation

--- expense.py
+++ expense.py
@@ -4,3 +4,7 @@ class Expense:
         self.name = name
         self.category = category
         self.amount = amount
+
+
+    def __repr__(self):
+        return f'<Expense: {self.name}, {self.category}, ${self.amount:.2f} >'
--- expense_tracker.py
+++ expense_tracker.py
@@ -1,12 +1,17 @@
+from expense import Expense
+
+
 def main():
 
+    
     print(f"running expense")
 
 
    
     # Get user input for expense
-    get_user_expense()
-
+    
+    expense = get_user_expense()
+    print(expense)
     # Write their expense into a file.
     save_user_expense()
 
@@ -20,7 +25,7 @@ def get_user_expense():
     print(f" Get user expense")
     expense_name = input('Enter expense name: ')
     expense_amount = float(input('Enter expense amount: '))
-    print(f"You have entered {expense_name}. {expense_amount}")
+   
 
 
     expense_categories = [
@@ -28,34 +33,40 @@ def get_user_expense():
     ]
 
     while True:
-        print('Select a category: ' )
+        print("Select a category: ")
         #Loops to show how many categories we previously defined
         for i, category_name in enumerate(expense_categories):
-            print(f' {i + 1}, {category_name}')
+            print(f"  {i + 1}. {category_name}")
 
 
         #variable for storing categories 
         value_range = f"[1 - {len(expense_categories)}]"
-
+      
 
         #Gets user input on what category number to choose
-        selected_index = int(input(f"Enter a category number: {value_range}: ")) -1 
+        selected_index = int(input(f"Enter a category number {value_range}: ")) - 1 
 
 
         #checks if user input is within the expense category length
 
         if selected_index in range(len(expense_categories)):
-            break
+            selected_category = expense_categories[selected_index]
+            new_expense = Expense(
+                name=expense_name, category=selected_category, amount=expense_amount
+            )
+            return new_expense
+        
+
         else:
             print("Invalid Category. Please Try again!")
 
 
-            
+        
     
 
 def save_user_expense():
     print(f" Save user expense")
-   
+    
 
 def summarize_user_expense():
     print(f" Summarize user expense")
